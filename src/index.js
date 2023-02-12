c15a8e84b35378bc34c00f1c222563eee838fc19
snowman95 <hun2ya9@gmail.com>
2023-02-12 11:24:41 +0900

    서버 주소를 변경하고 Apollo Client 에 자격증명 옵션 추가하라

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import GlobalStyles from "./styles";
 import Pages from "./pages";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+
+const SERVER_URL = "https://graphql-server.up.railway.app/";
 
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-part5-server-production-4c19.up.railway.app/", // change to YOUR own production server
+  uri: SERVER_URL, // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
+  link: new HttpLink({
+    uri: SERVER_URL,
+    credentials: "include",
+  }),
 });
 
 ReactDOM.render(
